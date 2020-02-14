529ccb171412440b594807524d9e3dfa0cad9568
arussell36 <andrewrussell036@gmail.com>
2020-02-13 18:30:10 -0600

    confirmation on json data pulling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Directory from "./Directory.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.Directory to the Directory json array
+  state = {
+    Directory
+  };
+
+  // Map over this.state.Directory and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Directory List</Title>
+        {this.state.Directory.map(Directory => (
+          <FriendCard
+            id={Directory.id}
+            key={Directory.id}
+            firstName={Directory.firstName}
+            lastName={Directory.lastName}
+            department={Directory.department}
+            title={Directory.title}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
