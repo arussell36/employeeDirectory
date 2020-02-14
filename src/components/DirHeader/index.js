cadb6863e10091d27dbf810a3413fd2c71d3c00b
arussell36 <andrewrussell036@gmail.com>
2020-02-13 21:42:26 -0600

    on click skeletons added

--- src/components/DirHeader/index.js
+++ src/components/DirHeader/index.js
@@ -2,18 +2,35 @@ import React from "react";
 import "./style.css";
 
 function DirHeader() {
+
+  function orderFirst() {
+    console.log('wowee were gonna order by first name')
+  }
+
+  function orderLast() {
+    console.log('whoa hoo order by last name')
+  }
+
+  function orderDept() {
+    console.log('interesting order by dept')
+  }
+
+  function orderTitle() {
+    console.log('hoke doke order by title')
+  }
+
   return (
     <>
-    <button className="headData" id="firstName">
+    <button className="headData" id="firstName" onClick={() => orderFirst()}>
         First Name
     </button>
-    <button className="headData" id="lastName">
+    <button className="headData" id="lastName" onClick={() => orderLast()}>
         Last Name
     </button>
-    <button className="headData" id="department">
+    <button className="headData" id="department" onClick={() => orderDept()}>
         Department
     </button>
-    <button className="headData" id="title">
+    <button className="headData" id="title" onClick={() => orderTitle()}>
         Title
     </button>
     </>
